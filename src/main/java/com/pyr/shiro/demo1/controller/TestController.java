e81956c50611762a86bae3147f1282250bd52d6b
panyuro <panyuro@163.com>
2023-02-26 19:38:50 +0800

    测试用户登录

---
+++ src/main/java/com/pyr/shiro/demo1/controller/TestController.java
@@ -0,0 +1,46 @@
+package com.pyr.shiro.demo1.controller;
+
+import com.pyr.shiro.demo1.model.User;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/loginUser")
+    public String loginUser(@RequestParam("username") String username,
+                            @RequestParam("password") String password,
+                            HttpSession session) {
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        Subject subject = SecurityUtils.getSubject();
+        try {
+            subject.login(token);
+            User user = (User) subject.getPrincipal();
+            session.setAttribute("user", user);
+            return "index";
+        } catch (Exception e) {
+            return "login";
+        }
+    }
+
+    @RequestMapping("unauthorized")
+    public String unauthorized() {
+        return "unauthorized";
+    }
+}
