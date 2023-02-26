87af7f9f96a55f7608a4cd700337ea58f595d343
panyuro <panyuro@163.com>
2023-02-26 20:34:55 +0800

    移除之前多提交的内容

--- src/main/java/com/pyr/shiro/demo1/AuthRealm.java
+++ src/main/java/com/pyr/shiro/demo1/AuthRealm.java
@@ -1,21 +1,14 @@
 package com.pyr.shiro.demo1;
 
 
-import com.pyr.shiro.demo1.model.Permission;
-import com.pyr.shiro.demo1.model.Role;
 import com.pyr.shiro.demo1.model.User;
 import com.pyr.shiro.demo1.service.UserService;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 public class AuthRealm extends AuthorizingRealm {
 
@@ -34,24 +27,24 @@ public class AuthRealm extends AuthorizingRealm {
     // 授权
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-        User user = (User) principals.fromRealm(this.getClass().getName()).iterator().next();
-        List<String> permissionList = new ArrayList<>();
-        List<String> roleNameList = new ArrayList<>();
-        Set<Role> roleSet = user.getRoles();
-        if (CollectionUtils.isNotEmpty(roleSet)) {
-            for(Role role : roleSet) {
-                roleNameList.add(role.getRname());
-                Set<Permission> permissionSet = role.getPermissions();
-                if (CollectionUtils.isNotEmpty(permissionSet)) {
-                    for (Permission permission : permissionSet) {
-                        permissionList.add(permission.getName());
-                    }
-                }
-            }
-        }
+        //User user = (User) principals.fromRealm(this.getClass().getName()).iterator().next();
+        //List<String> permissionList = new ArrayList<>();
+        //List<String> roleNameList = new ArrayList<>();
+        //Set<Role> roleSet = user.getRoles();
+        //if (CollectionUtils.isNotEmpty(roleSet)) {
+        //    for(Role role : roleSet) {
+        //        roleNameList.add(role.getRname());
+        //        Set<Permission> permissionSet = role.getPermissions();
+        //        if (CollectionUtils.isNotEmpty(permissionSet)) {
+        //            for (Permission permission : permissionSet) {
+        //                permissionList.add(permission.getName());
+        //            }
+        //        }
+        //    }
+        //}
         SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-        info.addStringPermissions(permissionList);
-        info.addRoles(roleNameList);
+        //info.addStringPermissions(permissionList);
+        //info.addRoles(roleNameList);
         return info;
     }
 
